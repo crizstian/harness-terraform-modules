3ffd802658d3d520557e28babe5a4a146bfad188
root <cristiano.rosetti@gmail.com>
2022-08-31 17:15:01 +0000

    harness-delegate k8s, docker support

--- harness-delegate/kubernetes.tf
+++ harness-delegate/kubernetes.tf
@@ -1,6 +1,10 @@
 resource "null_resource" "download_k8s_delegate_manifest" {
   for_each = local.k8s_delegates
 
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     working_dir = path.root
