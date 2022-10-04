615e82fb8470a6e80bb8d363501857e0dab5f946
root <cristiano.rosetti@gmail.com>
2022-10-04 04:06:10 +0000

    add pipeline/inputset module

--- harness-templates/variables.tf
+++ harness-templates/variables.tf
@@ -1,8 +1,11 @@
+variable "harness_templates" {}
+
 variable "harness_template_endpoint" {
   default = "https://app.harness.io/gateway/template/api/templates"
 }
-variable "harness_platform_api_key" {}
-variable "harness_templates" {}
+variable "harness_platform_api_key" {
+  default = ""
+}
 variable "harness_template_endpoint_account_args" {
   default = ""
 }
