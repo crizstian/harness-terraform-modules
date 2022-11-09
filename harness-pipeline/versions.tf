dd71098739ac174a07259ce953f7086ae4fa5c84
root <cristiano.rosetti@gmail.com>
2022-11-09 03:05:56 +0000

    add support for aws connector

--- harness-pipeline/versions.tf
+++ harness-pipeline/versions.tf
@@ -1,7 +1,7 @@
-terraform {
-  required_providers {
-    harness = {
-      source = "harness/harness"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     harness = {
+#       source = "harness/harness"
+#     }
+#   }
+# }
