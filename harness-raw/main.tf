a4b41cb3a537ce51afef32204a25ab03a6a0138b
root <cristiano.rosetti@gmail.com>
2022-11-25 05:04:51 +0000

    harness modules refactor + harness_raw module added

--- harness-raw/main.tf
+++ harness-raw/main.tf
@@ -9,7 +9,7 @@ resource "null_resource" "raw_request" {
     interpreter = ["/bin/bash", "-c"]
     command     = <<-EOT
         curl  -i -X ${each.value.request_type} '${each.value.endpoint}' \
-        --header 'Content-Type: application/yaml' \
+        --header 'Content-Type: application/${each.value.content_type}' \
         --header 'x-api-key: ${var.harness_platform_api_key}' -d '
         ${each.value.content}
         '
