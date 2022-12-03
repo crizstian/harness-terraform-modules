bf019bfdb1939ed05aa92f65587c795869390474
root <cristiano.rosetti@gmail.com>
2022-12-03 00:21:57 +0000

    refactor delegate module

---
+++ harness-delegate/output.tf
@@ -0,0 +1,20 @@
+output "delegate_init" {
+  value = {
+    service_ref     = var.enable_delegate_init_service ? harness_platform_service.service[0].identifier : ""
+    environment_ref = var.enable_delegate_init_service ? harness_platform_environment.environment[0].identifier : ""
+  }
+}
+
+output "manifests" {
+  value = {
+    "${local.harness_organization_id}" = { for key, value in local.delegates : key => {
+      identifier = value.identifier
+      manifest   = "${local.harness_filestore_api}/files/${value.identifier}/download?${value.url_args}"
+      file_store = "https://app.harness.io/ng/#/account/${var.harness_account_id}/settings/resources/file-store"
+    } }
+  }
+}
+
+output "delegates-verbose" {
+  value = local.delegates
+}
