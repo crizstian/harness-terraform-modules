19ca714bfb2c2df2f77c3e09e53f86925c03b589
root <cristiano.rosetti@gmail.com>
2022-12-05 02:41:02 +0000

    refactor k8s connector

--- harness-delegate/variables.tf
+++ harness-delegate/variables.tf
@@ -1,7 +1,9 @@
 variable "harness_platform_api_key" {}
 variable "harness_platform_delegates" {}
 variable "harness_account_id" {}
-variable "harness_organization" {}
+variable "harness_organization" {
+  default = {}
+}
 variable "suffix" {}
 variable "enable_delegate_init_service" {}
 variable "harness_api_endpoint" {
@@ -22,8 +24,8 @@ variable "tags" {
 
 # Common Vars
 locals {
-  harness_organization_id         = var.harness_organization.org_id
-  harness_organization_project_id = var.harness_organization.seed_project_id
+  harness_organization_id         = try(var.harness_organization.org_id, "default")
+  harness_organization_project_id = try(var.harness_organization.seed_project_id, "")
   harness_filestore_api           = "${var.harness_api_endpoint}/file-store"
   account_args                    = "accountIdentifier=${var.harness_account_id}"
 }
@@ -49,15 +51,15 @@ locals {
   docker_delegates = { for name, delegate in try(local.enabled_delegates.docker, {}) : name => {
     manifest          = "${name}-${var.delegate_manifest}"
     delegate_endpoint = "${var.harness_api_endpoint}/download-delegates/docker"
-    url_args          = can(delegate.org_id) ? can(delegate.proj_id) ? "${local.account_args}&orgIdentifier=${delegate.org_id}&projectIdentifier=${delegate.proj_id}" : "${local.account_args}&orgIdentifier=${delegate.org_id}" : "${local.account_args}"
-    tokenName         = can(delegate.tokenName) ? delegate.tokenName : can(delegate.org_id) ? "default_token_${delegate.org_id}" : "default_token"
+    url_args          = can(delegate.org_id) ? can(delegate.proj_id) ? "${local.account_args}&orgIdentifier=${try(delegate.org_id, local.harness_organization_id)}&projectIdentifier=${delegate.proj_id}" : "${local.account_args}&orgIdentifier=${try(delegate.org_id, local.harness_organization_id)}" : "${local.account_args}"
+    tokenName         = can(delegate.tokenName) ? delegate.tokenName : can(delegate.org_id) ? "default_token_${try(delegate.org_id, local.harness_organization_id)}" : "default_token"
     identifier        = "${lower(replace(name, "/[\\s-.]/", "_"))}_${var.suffix}"
     auto_install      = try(delegate.auto_install, false)
     connection        = try(delegate.connection, {})
 
     body = jsonencode({
       name                   = name
-      tokenName              = can(delegate.tokenName) ? delegate.tokenName : can(delegate.org_id) ? "default_token_${delegate.org_id}" : "default_token"
+      tokenName              = can(delegate.tokenName) ? delegate.tokenName : can(delegate.org_id) ? "default_token_${try(delegate.org_id, local.harness_organization_id)}" : "default_token"
       description            = delegate.description
       size                   = delegate.size
       tags                   = delegate.tags
@@ -68,9 +70,9 @@ locals {
 
   k8s_delegates = { for name, delegate in try(local.enabled_delegates.k8s, {}) : name => {
     manifest          = "${name}-${var.delegate_manifest}"
-    url_args          = can(delegate.org_id) ? can(delegate.proj_id) ? "${local.account_args}&orgIdentifier=${delegate.org_id}&projectIdentifier=${delegate.proj_id}" : "${local.account_args}&orgIdentifier=${delegate.org_id}" : "${local.account_args}"
+    url_args          = can(delegate.org_id) ? can(delegate.proj_id) ? "${local.account_args}&orgIdentifier=${try(delegate.org_id, local.harness_organization_id)}&projectIdentifier=${delegate.proj_id}" : "${local.account_args}&orgIdentifier=${try(delegate.org_id, local.harness_organization_id)}" : "${local.account_args}"
     delegate_endpoint = "${var.harness_api_endpoint}/download-delegates/kubernetes"
-    tokenName         = can(delegate.tokenName) ? delegate.tokenName : can(delegate.org_id) ? "default_token_${delegate.org_id}" : "default_token"
+    tokenName         = can(delegate.tokenName) ? delegate.tokenName : can(delegate.org_id) ? "default_token_${try(delegate.org_id, local.harness_organization_id)}" : "default_token"
     identifier        = "${lower(replace(name, "/[\\s-.]/", "_"))}_${var.suffix}"
     auto_install      = try(delegate.auto_install, false)
     connection        = try(delegate.connection, {})
@@ -80,7 +82,7 @@ locals {
       description            = delegate.description
       size                   = delegate.size
       tags                   = delegate.tags
-      tokenName              = can(delegate.tokenName) ? delegate.tokenName : can(delegate.org_id) ? "default_token_${delegate.org_id}" : "default_token"
+      tokenName              = can(delegate.tokenName) ? delegate.tokenName : can(delegate.org_id) ? "default_token_${try(delegate.org_id, local.harness_organization_id)}" : "default_token"
       clusterPermissionType  = delegate.clusterPermissionType
       customClusterNamespace = delegate.customClusterNamespace
     })
@@ -93,7 +95,7 @@ locals {
     for key, value in delegates : key => merge(
       value,
       {
-        org_id     = try(value.org_id, ""),
+        org_id     = try(delegate.org_id, local.harness_organization_id)
         project_id = try(value.project_id, "")
         enable     = value.create_connector
     })
