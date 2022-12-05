8e24e6b9d97a03cfa037fedd707fd71e2c964463
root <cristiano.rosetti@gmail.com>
2022-12-05 02:01:28 +0000

    refactor k8s connector

--- harness-delegate/output.tf
+++ harness-delegate/output.tf
@@ -8,10 +8,10 @@ output "delegate_init" {
 output "manifests" {
   value = {
     "${local.harness_organization_id}" = { for key, value in local.delegates : key => {
-      identifier     = value.identifier
-      manifest       = "${local.harness_filestore_api}/files/${value.identifier}/download?${value.url_args}"
-      file_store     = "https://app.harness.io/ng/#/account/${var.harness_account_id}/settings/resources/file-store"
-      k8s_connectors = { identifier = module.bootstrap_harness_connectors[key].connectors.k8s_connectors["${key}_k8s_connector"].identifier }
+      identifier    = value.identifier
+      manifest      = "${local.harness_filestore_api}/files/${value.identifier}/download?${value.url_args}"
+      file_store    = "https://app.harness.io/ng/#/account/${var.harness_account_id}/settings/resources/file-store"
+      k8s_connector = { identifier = module.bootstrap_harness_connectors[key].connectors.k8s_connectors["${key}_k8s_connector"].identifier }
     } }
   }
 }
