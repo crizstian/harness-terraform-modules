953d24b557267dc2d0d34a7bfb0fe78c920c9f5c
root <cristiano.rosetti@gmail.com>
2022-12-05 01:28:09 +0000

    refactor k8s connector

--- harness-delegate/connector.tf
+++ harness-delegate/connector.tf
@@ -3,7 +3,7 @@ module "bootstrap_harness_connectors" {
   for_each                        = local.k8s_connectors
   suffix                          = var.suffix
   tags                            = var.tags
-  org_id                          = each.org_id
-  project_id                      = each.project_id
+  org_id                          = each.value.org_id
+  project_id                      = each.value.project_id
   harness_platform_k8s_connectors = { "${each.key}" = each.value }
 }
