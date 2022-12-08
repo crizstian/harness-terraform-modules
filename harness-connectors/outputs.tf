1eac3c302f869ccd13706739768617475feb1379
root <cristiano.rosetti@gmail.com>
2022-12-08 15:56:07 +0000

    refactor connectors

--- harness-connectors/outputs.tf
+++ harness-connectors/outputs.tf
@@ -8,7 +8,7 @@ locals {
     },
     { for key, value in harness_platform_connector_github.connector : key =>
       {
-        identifier = value.project_id != "" ? value.identifier : value.org_id != "default" ? "org.${value.identifier}" : "account.${value.identifier}"
+        identifier = value.project_id != "" ? value.identifier : value.org_id != "" ? "org.${value.identifier}" : "account.${value.identifier}"
       }
     }
   )
@@ -20,7 +20,7 @@ locals {
     },
     { for key, value in harness_platform_connector_docker.connector : key =>
       {
-        identifier = value.project_id != "" ? value.identifier : value.org_id != "default" ? "org.${value.identifier}" : "account.${value.identifier}"
+        identifier = value.project_id != "" ? value.identifier : value.org_id != "" ? "org.${value.identifier}" : "account.${value.identifier}"
       }
     }
   )
@@ -38,12 +38,12 @@ locals {
   )
   aws_connectors_output = { for key, value in harness_platform_connector_aws.connector : key =>
     {
-      identifier = value.project_id != "" ? value.identifier : value.org_id != "default" ? "org.${value.identifier}" : "account.${value.identifier}"
+      identifier = value.project_id != "" ? value.identifier : value.org_id != "" ? "org.${value.identifier}" : "account.${value.identifier}"
     }
   }
   gcp_connectors_output = { for key, value in harness_platform_connector_gcp.connector : key =>
     {
-      identifier = value.project_id != "" ? value.identifier : value.org_id != "default" ? "org.${value.identifier}" : "account.${value.identifier}"
+      identifier = value.project_id != "" ? value.identifier : value.org_id != "" ? "org.${value.identifier}" : "account.${value.identifier}"
     }
   }
 }
