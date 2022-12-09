9e3257a5a0c020a9b02aa2ca5d50a419a6c38c95
root <cristiano.rosetti@gmail.com>
2022-12-09 01:47:07 +0000

    adding trigger resource

--- harness-templates/main.tf
+++ harness-templates/main.tf
@@ -1,19 +1,7 @@
-data "local_file" "read_template" {
-  for_each = var.harness_templates
-  filename = each.value.file
-}
-
 resource "local_file" "template" {
-  depends_on = [
-    data.local_file.read_template,
-  ]
   for_each = var.harness_templates
   content  = templatefile(each.value.file, merge(each.value.vars, { name = each.key }))
   filename = "${path.module}/${each.key}.yml"
-
-  lifecycle {
-    replace_triggered_by = [data.local_file.read_template[each.key].content]
-  }
 }
 
 data "local_file" "template" {
