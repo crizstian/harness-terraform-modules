2b0e0dbfe9a7204e2d29c4f279b5d63c6dd66775
root <cristiano.rosetti@gmail.com>
2022-12-12 04:21:58 +0000

    refactor

--- harness-pipeline/output.tf
+++ harness-pipeline/output.tf
@@ -1,8 +1,21 @@
-output "pipelines" {
-  value = { for key, details in harness_platform_pipeline.pipeline : key => merge(
+locals {
+  inputset_output = merge([for pipeline, details in harness_platform_pipeline.pipeline : {
+    for key, input in harness_platform_input_set.inputset : key => { identifier = input.identifier } if input.pipeline_id == details.identifier
+    }
+  ]...)
+  trigger_output = merge([for pipeline, details in harness_platform_pipeline.pipeline : {
+    for key, trigger in harness_platform_triggers.trigger : key => { identifier = trigger.identifier } if trigger.target_id == details.identifier
+    }
+  ]...)
+
+  pipeline_output = { for key, details in harness_platform_pipeline.pipeline : key => merge(
     { pipeline_id = details.identifier },
-    length(keys(harness_platform_input_set.inputset)) > 0 ? { inputsets = { for key, input in harness_platform_input_set.inputset : key => { identifier = input.identifier } if input.pipeline_id == details.identifier } } : {},
-    length(keys(harness_platform_triggers.trigger)) > 0 ? { triggers = { for key, trigger in harness_platform_triggers.trigger : key => { identifier = trigger.identifier } if trigger.target_id == details.identifier } } : {}
+    length(keys(local.inputset_output)) > 0 ? { inputsets = local.inputset_output } : {},
+    length(keys(local.trigger_output)) > 0 ? { triggers = local.trigger_output } : {}
     )
   }
+}
+
+output "pipelines" {
+  value = local.pipeline_output
 }
