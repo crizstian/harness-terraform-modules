e1a524c4ec6b8b9df95afdeb9eb3d4e46f6afc61
root <cristiano.rosetti@gmail.com>
2022-12-12 04:42:35 +0000

    refactor

--- harness-pipeline/output.tf
+++ harness-pipeline/output.tf
@@ -1,17 +1,17 @@
 locals {
-  inputset_output = merge([for pipeline, details in harness_platform_pipeline.pipeline : {
+  inputset_output = { for pipeline, details in harness_platform_pipeline.pipeline : pipeline => {
     for key, input in harness_platform_input_set.inputset : key => { identifier = input.identifier } if input.pipeline_id == details.identifier
     }
-  ]...)
-  trigger_output = merge([for pipeline, details in harness_platform_pipeline.pipeline : {
+  }
+  trigger_output = { for pipeline, details in harness_platform_pipeline.pipeline : pipeline => {
     for key, trigger in harness_platform_triggers.trigger : key => { identifier = trigger.identifier } if trigger.target_id == details.identifier
     }
-  ]...)
+  }
 
   pipeline_output = { for key, details in harness_platform_pipeline.pipeline : key => merge(
     { pipeline_id = details.identifier },
-    length(keys(local.inputset_output)) > 0 ? { inputsets = local.inputset_output } : {},
-    length(keys(local.trigger_output)) > 0 ? { triggers = local.trigger_output } : {}
+    length(keys(local.inputset_output[key])) > 0 ? { inputsets = local.inputset_output[key] } : {},
+    length(keys(local.trigger_output[key])) > 0 ? { triggers = local.trigger_output[key] } : {}
     )
   }
 }
