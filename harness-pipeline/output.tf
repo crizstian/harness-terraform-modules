09295e0d157631e075c5ac7501ee9dc6b475030d
root <cristiano.rosetti@gmail.com>
2022-12-12 03:44:22 +0000

    refactor

---
+++ harness-pipeline/output.tf
@@ -0,0 +1,8 @@
+output "pipelines" {
+  value = { for key, details in harness_platform_pipeline.pipeline : key => merge(
+    { pipeline_id = details.identifier },
+    length(keys(harness_platform_input_set.inputset)) > 0 ? { inputsets = { for key, input in harness_platform_input_set.inputset : key => { identifier = input.identifier } if input.pipeline_id == details.identifier } } : {},
+    length(keys(harness_platform_triggers.trigger)) > 0 ? { triggers = { for key, trigger in harness_platform_triggers.trigger : key => { identifier = trigger.identifier } if input.pipeline_id == details.identifier } } : {}
+    )
+  }
+}
