66f2a54383780fed4e56352fd5b171d39bc2e276
root <cristiano.rosetti@gmail.com>
2022-12-29 00:22:08 +0000

    adding git harness native support for pipelines + adding harness template module

--- harness-template/variables.tf
+++ harness-template/variables.tf
@@ -6,6 +6,13 @@ variable "harness_platform_templates" {
   description = "Harness Templates to be created in the given Harness account"
   default     = {}
 }
+variable "org_id" {
+  default = ""
+}
+variable "project_id" {
+  default = ""
+}
+
 
 locals {
   harness_templates = { for name, details in var.harness_platform_templates : name => merge(
@@ -14,6 +21,8 @@ locals {
       vars = merge(
         details.vars,
         {
+          org_id      = try(details.org_id, var.org_id)
+          project_id  = try(details.project_id, var.project_id)
           tags        = concat(details.tags, var.tags)
           git_details = try(details.vars.git_details, {})
           identifier  = "${lower(replace(name, "-", "_"))}_template_${var.suffix}"
