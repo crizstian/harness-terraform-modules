8c6db2698880be01f7b32ffa541e3713b7c63014
root <cristiano.rosetti@gmail.com>
2023-01-09 21:46:16 +0000

    update

--- harness-project/variables.tf
+++ harness-project/variables.tf
@@ -12,6 +12,9 @@ variable "suffix" {}
 variable "tags" {
   default = []
 }
+variable "org_id" {
+  default = "default"
+}
 
 locals {
   orgs = { for name, details in var.harness_platform_organizations : name => merge(
@@ -29,7 +32,7 @@ locals {
     {
       identifier = "${lower(replace(name, "/[\\s-.]/", "_"))}_${var.suffix}"
       name       = try(details.short_name, "terraform")
-      org_id     = try(details.org_id, "default")
+      org_id     = try(details.org_id, var.org_id)
       tags       = concat(try(details.tags, []), var.tags)
     })
     if details.enable
