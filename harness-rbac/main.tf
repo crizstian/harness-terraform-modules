34f47b01f114a034e8a05babc7e014c6c42e9ecc
root <cristiano.rosetti@gmail.com>
2023-03-15 00:04:11 +0000

    adding rbac module

--- harness-rbac/main.tf
+++ harness-rbac/main.tf
@@ -1,7 +1,4 @@
-# Create user at project level
-resource "harness_platform_user" "user" {
-  org_id      = "org_id"
-  project_id  = "project_id"
+resource "harness_platform_user" "example" {
   email       = "john.doe@harness.io"
   user_groups = ["_project_all_users"]
   role_bindings {
