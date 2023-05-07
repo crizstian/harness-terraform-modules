e1e38c703e26a6e6a5765edb34fad950bb875af4
root <cristiano.rosetti@gmail.com>
2023-05-07 23:24:38 +0000

    connectors

--- harness-connector/main.tf
+++ harness-connector/main.tf
@@ -1,3 +1,159 @@
+resource "harness_platform_connector_gitlab" "connector" {
+  for_each           = local.gitlab_connectors
+  identifier         = each.value.identifier
+  name               = each.key
+  description        = each.value.description
+  url                = each.value.url
+  connection_type    = each.value.connection_type
+  validation_repo    = each.value.validation_repo
+  org_id             = each.value.org_id
+  project_id         = each.value.project_id
+  tags               = each.value.tags
+  delegate_selectors = each.value.delegate_selectors
+
+  credentials {
+    http {
+      username  = each.value.credentials.http.username
+      token_ref = each.value.credentials.http.token_ref_id
+    }
+  }
+  api_authentication {
+    token_ref = each.value.credentials.http.token_ref_id
+  }
+}
+
+resource "harness_platform_connector_artifactory" "connector" {
+  for_each           = local.artifactory_connectors
+  identifier         = each.value.identifier
+  name               = each.key
+  description        = each.value.description
+  url                = each.value.url
+  org_id             = each.value.org_id
+  project_id         = each.value.project_id
+  tags               = each.value.tags
+  delegate_selectors = each.value.delegate_selectors
+
+  credentials {
+    username     = each.value.credentials.http.username
+    password_ref = each.value.credentials.http.password_ref_id
+  }
+}
+
+resource "harness_platform_connector_gcp" "connector" {
+  for_each    = local.gcp_connectors
+  identifier  = each.value.identifier
+  name        = each.key
+  description = each.value.description
+  tags        = each.value.tags
+  project_id  = each.value.project_id
+  org_id      = each.value.org_id
+
+  dynamic "manual" {
+    for_each = each.value.manual
+    content {
+      secret_key_ref     = manual.value.secret_key_ref
+      delegate_selectors = manual.value.delegate_selectors
+    }
+  }
+
+  dynamic "inherit_from_delegate" {
+    for_each = each.value.inherit_from_delegate
+    content {
+      delegate_selectors = inherit_from_delegate.value.delegate_selectors
+    }
+  }
+}
+
+resource "harness_platform_connector_nexus" "connector" {
+  for_each           = local.nexus_connectors
+  identifier         = each.value.identifier
+  name               = each.key
+  description        = each.value.description
+  url                = each.value.url
+  org_id             = each.value.org_id
+  project_id         = each.value.project_id
+  tags               = each.value.tags
+  delegate_selectors = each.value.delegate_selectors
+  version            = each.value.version
+
+  dynamic "credentials" {
+    for_each = each.value.credentials
+    content {
+      username     = credentials.value.username
+      password_ref = credentials.value.password_ref_id
+    }
+  }
+}
+
+resource "harness_platform_connector_service_now" "connector" {
+  for_each           = local.service_now_connectors
+  identifier         = each.value.identifier
+  name               = each.key
+  description        = each.value.description
+  org_id             = each.value.org_id
+  project_id         = each.value.project_id
+  tags               = each.value.tags
+  delegate_selectors = each.value.delegate_selectors
+  service_now_url    = each.value.service_now_url
+
+  auth {
+    auth_type = each.value.auth.credentials.auth_type
+
+    username_password {
+      username     = each.value.auth.credentials.username
+      password_ref = each.value.auth.credentials.password_ref
+    }
+  }
+}
+
+resource "harness_platform_connector_dynatrace" "connector" {
+  for_each           = local.dynatrace_connectors
+  identifier         = each.value.identifier
+  name               = each.key
+  description        = each.value.description
+  org_id             = each.value.org_id
+  project_id         = each.value.project_id
+  tags               = each.value.tags
+  delegate_selectors = each.value.delegate_selectors
+  url                = each.value.url
+  api_token_ref      = each.value.api_token_ref
+}
+
+resource "harness_platform_connector_kubernetes" "connector" {
+  for_each           = local.kubernetes_connectors
+  identifier         = each.value.identifier
+  name               = each.key
+  description        = each.value.description
+  tags               = each.value.tags
+  project_id         = each.value.project_id
+  org_id             = each.value.org_id
+  delegate_selectors = each.value.delegate_selectors
+
+  dynamic "service_account" {
+    for_each = each.value.service_account
+    content {
+      master_url                = service_account.value.master_url
+      service_account_token_ref = service_account.value.service_account_token_ref
+    }
+  }
+
+  dynamic "username_password" {
+    for_each = each.value.username_password
+    content {
+      master_url   = username_password.value.master_url
+      username     = username_password.value.username
+      password_ref = username_password.value.password_ref
+    }
+  }
+
+  dynamic "inherit_from_delegate" {
+    for_each = each.value.inherit_from_delegate
+    content {
+      delegate_selectors = inherit_from_delegate.value.delegate_selectors
+    }
+  }
+}
+
 resource "harness_platform_connector_github" "connector" {
   for_each        = local.github_connectors
   identifier      = each.value.identifier
@@ -39,32 +195,6 @@ resource "harness_platform_connector_docker" "connector" {
   }
 }
 
-resource "harness_platform_connector_kubernetes" "connector" {
-  for_each    = local.k8s_connectors
-  identifier  = each.value.identifier
-  name        = each.key
-  description = each.value.description
-  tags        = each.value.tags
-  project_id  = each.value.project_id
-  org_id      = each.value.org_id
-
-  inherit_from_delegate {
-    delegate_selectors = each.value.delegate_selectors
-  }
-
-  service_account {
-    master_url                = "https://kubernetes.example.com"
-    service_account_token_ref = "account.TEST_k8s_client_test"
-  }
-  delegate_selectors = ["harness-delegate"]
-
-  username_password {
-    master_url   = "https://kubernetes.example.com"
-    username     = "admin"
-    password_ref = "account.TEST_k8s_client_test"
-  }
-}
-
 resource "harness_platform_connector_aws" "connector" {
   for_each    = local.aws_connectors
   identifier  = each.value.identifier
@@ -80,38 +210,4 @@ resource "harness_platform_connector_aws" "connector" {
     delegate_selectors = each.value.delegate_selectors
   }
 }
-
-resource "harness_platform_connector_gcp" "connector" {
-  for_each    = local.gcp_connectors
-  identifier  = each.value.identifier
-  name        = each.key
-  description = each.value.description
-  tags        = each.value.tags
-  project_id  = each.value.project_id
-  org_id      = each.value.org_id
-
-  manual {
-    secret_key_ref     = each.value.manual.secret_key_ref
-    delegate_selectors = each.value.delegate_selectors
-  }
-}
-
-# resource "harness_platform_connector_artifactory" "connector" {
-#   for_each           = local.artifactory_connectors
-#   identifier         = "${lower(replace(name, "/[\\s-.]/", "_"))}_${var.suffix}"
-#   name               = each.key
-#   description        = each.value.description
-#   url                = each.value.url
-#   connection_type    = each.value.connection_type
-#   validation_repo    = each.value.validation_repo
-#   delegate_selectors = each.value.delegate_selectors
-#   org_id             = each.value.org_id
-#   #   project_id         = each.value.project_id
-#   credentials {
-#     http {
-#       username     = each.value.credentials.http.username
-#       password_ref = harness_platform_secret_text.harness_secrets["${each.key}_secret"].id
-#     }
-#   }
-# }
 
--- harness-connector/outputs.tf
+++ harness-connector/outputs.tf
@@ -1,5 +1,29 @@
 # Outputs
 locals {
+  gitlab_connectors_output = merge(
+    { for name, details in var.harness_platform_gitlab_connectors : name =>
+      {
+        identifier = details.id
+      } if !details.enable && can(details.id)
+    },
+    { for key, value in harness_platform_connector_gitlab.connector : key =>
+      {
+        identifier = value.project_id != "" ? value.identifier : value.org_id != "" ? "org.${value.identifier}" : "account.${value.identifier}"
+      }
+    }
+  )
+  artifactory_connectors_output = merge(
+    { for name, details in var.harness_platform_artifactory_connectors : name =>
+      {
+        identifier = details.id
+      } if !details.enable && can(details.id)
+    },
+    { for key, value in harness_platform_connector_artifactory.connector : key =>
+      {
+        identifier = value.project_id != "" ? value.identifier : value.org_id != "" ? "org.${value.identifier}" : "account.${value.identifier}"
+      }
+    }
+  )
   github_connectors_output = merge(
     { for name, details in var.harness_platform_github_connectors : name =>
       {
@@ -24,24 +48,32 @@ locals {
       }
     }
   )
-  k8s_connectors_output = merge(
-    { for name, details in var.harness_platform_k8s_connectors : name =>
-      {
-        identifier = details.connector_id
-      } if !details.enable && can(details.connector_id)
-    },
-    { for key, value in harness_platform_connector_kubernetes.connector : key =>
-      {
-        identifier = value.project_id != "" ? value.identifier : value.org_id != "" ? "org.${value.identifier}" : "account.${value.identifier}"
-      }
-    }
-  )
-  aws_connectors_output = { for key, value in harness_platform_connector_aws.connector : key =>
+  gcp_connectors_output = { for key, value in harness_platform_connector_gcp.connector : key =>
     {
       identifier = value.project_id != "" ? value.identifier : value.org_id != "" ? "org.${value.identifier}" : "account.${value.identifier}"
     }
   }
-  gcp_connectors_output = { for key, value in harness_platform_connector_gcp.connector : key =>
+  nexus_connectors_output = { for key, value in harness_platform_connector_nexus.connector : key =>
+    {
+      identifier = value.project_id != "" ? value.identifier : value.org_id != "" ? "org.${value.identifier}" : "account.${value.identifier}"
+    }
+  }
+  service_now_connectors_output = { for key, value in harness_platform_connector_service_now.connector : key =>
+    {
+      identifier = value.project_id != "" ? value.identifier : value.org_id != "" ? "org.${value.identifier}" : "account.${value.identifier}"
+    }
+  }
+  dynatrace_connectors_output = { for key, value in harness_platform_connector_dynatrace.connector : key =>
+    {
+      identifier = value.project_id != "" ? value.identifier : value.org_id != "" ? "org.${value.identifier}" : "account.${value.identifier}"
+    }
+  }
+  kubernetes_connectors_output = { for key, value in harness_platform_connector_kubernetes.connector : key =>
+    {
+      identifier = value.project_id != "" ? value.identifier : value.org_id != "" ? "org.${value.identifier}" : "account.${value.identifier}"
+    }
+  }
+  aws_connectors_output = { for key, value in harness_platform_connector_aws.connector : key =>
     {
       identifier = value.project_id != "" ? value.identifier : value.org_id != "" ? "org.${value.identifier}" : "account.${value.identifier}"
     }
@@ -50,10 +82,15 @@ locals {
 
 output "connectors" {
   value = merge(
+    length(keys(local.gitlab_connectors_output)) > 0 ? { gitlab_connectors = local.gitlab_connectors_output } : {},
+    length(keys(local.artifactory_connectors_output)) > 0 ? { artifactory_connectors = local.artifactory_connectors_output } : {},
     length(keys(local.github_connectors_output)) > 0 ? { github_connectors = local.github_connectors_output } : {},
     length(keys(local.docker_connectors_output)) > 0 ? { docker_connectors = local.docker_connectors_output } : {},
-    length(keys(local.aws_connectors_output)) > 0 ? { aws_connectors = local.aws_connectors_output } : {},
     length(keys(local.gcp_connectors_output)) > 0 ? { gcp_connectors = local.gcp_connectors_output } : {},
-    length(keys(local.k8s_connectors_output)) > 0 ? { k8s_connectors = local.k8s_connectors_output } : {}
+    length(keys(local.nexus_connectors_output)) > 0 ? { nexus_connectors = local.nexus_connectors_output } : {},
+    length(keys(local.service_now_connectors_output)) > 0 ? { service_now_connectors = local.service_now_connectors_output } : {},
+    length(keys(local.dynatrace_connectors_output)) > 0 ? { dynatrace_connectors = local.dynatrace_connectors_output } : {},
+    length(keys(local.kubernetes_connectors_output)) > 0 ? { kubernetes_connectors = local.kubernetes_connectors_output } : {},
+    length(keys(local.aws_connectors_output)) > 0 ? { aws_connectors = local.aws_connectors_output } : {}
   )
 }
