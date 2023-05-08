9118fcf39c25bca40246168e694ef811fc835820
root <cristiano.rosetti@gmail.com>
2023-05-08 01:29:15 +0000

    template

--- harness-template/main.tf
+++ harness-template/main.tf
@@ -1,30 +1,118 @@
-# Renders harness template files
-module "render_harness_template_files" {
-  source            = "../terraform-templates"
-  harness_templates = local.harness_templates
-}
-
-# Loads harness template files in order to provision it with terraform
-data "local_file" "harness_template" {
-  depends_on = [
-    module.render_harness_template_files
-  ]
-  for_each = local.harness_templates
-  filename = module.render_harness_template_files.files[each.key]
-}
-
-resource "harness_platform_template" "template" {
-  for_each      = local.harness_templates
+resource "harness_platform_template" "step" {
+  for_each      = local.steps
   name          = each.key
   description   = each.value.vars.description
   identifier    = each.value.vars.identifier
   org_id        = each.value.vars.org_id
   project_id    = each.value.vars.project_id
   tags          = each.value.vars.tags
-  comments      = each.value.vars.comments
   version       = each.value.vars.version
+  comments      = each.value.vars.comments
   is_stable     = each.value.vars.is_stable
-  template_yaml = data.local_file.harness_template[each.key].content
+  template_yaml = templatefile(each.value.vars.yaml, each.value.vars)
+
+  dynamic "git_details" {
+    for_each = each.value.vars.git_details
+    content {
+      branch_name    = git_details.value["branch_name"]
+      commit_message = git_details.value["commit_message"]
+      file_path      = git_details.value["file_path"]
+      connector_ref  = git_details.value["connector_ref"]
+      store_type     = git_details.value["store_type"]
+      repo_name      = git_details.value["repo_name"]
+    }
+  }
+}
+resource "harness_platform_template" "stage" {
+  depends_on    = [harness_platform_template.step]
+  for_each      = local.stages
+  name          = each.key
+  description   = each.value.vars.description
+  identifier    = each.value.vars.identifier
+  org_id        = each.value.vars.org_id
+  project_id    = each.value.vars.project_id
+  tags          = each.value.vars.tags
+  version       = each.value.vars.version
+  comments      = each.value.vars.comments
+  is_stable     = each.value.vars.is_stable
+  template_yaml = templatefile(each.value.vars.yaml, each.value.vars)
+
+  dynamic "git_details" {
+    for_each = each.value.vars.git_details
+    content {
+      branch_name    = git_details.value["branch_name"]
+      commit_message = git_details.value["commit_message"]
+      file_path      = git_details.value["file_path"]
+      connector_ref  = git_details.value["connector_ref"]
+      store_type     = git_details.value["store_type"]
+      repo_name      = git_details.value["repo_name"]
+    }
+  }
+}
+resource "harness_platform_template" "template_deployment" {
+  for_each      = local.template_deployment
+  name          = each.key
+  description   = each.value.vars.description
+  identifier    = each.value.vars.identifier
+  org_id        = each.value.vars.org_id
+  project_id    = each.value.vars.project_id
+  tags          = each.value.vars.tags
+  version       = each.value.vars.version
+  comments      = each.value.vars.comments
+  is_stable     = each.value.vars.is_stable
+  template_yaml = templatefile(each.value.vars.yaml, each.value.vars)
+
+  dynamic "git_details" {
+    for_each = each.value.vars.git_details
+    content {
+      branch_name    = git_details.value["branch_name"]
+      commit_message = git_details.value["commit_message"]
+      file_path      = git_details.value["file_path"]
+      connector_ref  = git_details.value["connector_ref"]
+      store_type     = git_details.value["store_type"]
+      repo_name      = git_details.value["repo_name"]
+    }
+  }
+}
+resource "harness_platform_template" "pipeline" {
+  depends_on    = [harness_platform_template.stage, harness_platform_template.template_deployment]
+  for_each      = local.pipelines
+  name          = each.key
+  description   = each.value.vars.description
+  identifier    = each.value.vars.identifier
+  org_id        = each.value.vars.org_id
+  project_id    = each.value.vars.project_id
+  tags          = each.value.vars.tags
+  version       = each.value.vars.version
+  comments      = each.value.vars.comments
+  is_stable     = each.value.vars.is_stable
+  template_yaml = templatefile(each.value.vars.yaml, each.value.vars)
+
+  dynamic "git_details" {
+    for_each = each.value.vars.git_details
+    content {
+      branch_name    = git_details.value["branch_name"]
+      commit_message = git_details.value["commit_message"]
+      file_path      = git_details.value["file_path"]
+      connector_ref  = git_details.value["connector_ref"]
+      store_type     = git_details.value["store_type"]
+      repo_name      = git_details.value["repo_name"]
+    }
+  }
+}
+resource "harness_platform_template" "template" {
+  for_each      = local.definitions
+  name          = each.key
+  description   = each.value.vars.description
+  identifier    = each.value.vars.identifier
+  org_id        = each.value.vars.org_id
+  project_id    = each.value.vars.project_id
+  tags          = each.value.vars.tags
+  version       = each.value.vars.version
+  comments      = each.value.vars.comments
+  is_stable     = each.value.vars.is_stable
+  template_yaml = templatefile(each.value.vars.yaml, each.value.vars)
+
   dynamic "git_details" {
     for_each = each.value.vars.git_details
     content {
--- harness-template/variables.tf
+++ harness-template/variables.tf
@@ -1,33 +1,22 @@
-variable "suffix" {}
-variable "tags" {
-  default = []
-}
 variable "harness_platform_templates" {
-  description = "Harness Templates to be created in the given Harness account"
-  default     = {}
+  default = {}
 }
+variable "suffix" {}
 variable "org_id" {
   default = ""
 }
 variable "project_id" {
   default = ""
 }
-
-
-locals {
-  harness_templates = { for name, details in var.harness_platform_templates : name => merge(
-    details,
-    {
-      vars = merge(
-        details.vars,
-        {
-          org_id      = try(details.org_id, var.org_id)
-          project_id  = try(details.project_id, var.project_id)
-          tags        = concat(details.tags, var.tags)
-          git_details = try(details.vars.git_details, {})
-          identifier  = "${lower(replace(name, "-", "_"))}_template_${var.suffix}"
-          description = details.description
-      })
-    })
-  }
+variable "tags" {
+  default = ""
+}
+variable "organizations" {
+  default = {}
+}
+variable "projects" {
+  default = {}
+}
+variable "connectors" {
+  default = {}
 }
