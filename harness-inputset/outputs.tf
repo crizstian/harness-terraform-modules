be8ec1659f10c09f036d79c3063b56c850f2b6c2
root <cristiano.rosetti@gmail.com>
2023-05-09 18:00:08 +0000

    inputset

--- harness-inputset/outputs.tf
+++ harness-inputset/outputs.tf
@@ -1,14 +1,15 @@
 output "inputset" {
-  value = { for key, details in harness_platform_input_set.inputset : key => {
-    identifier = details.identifier
-    org_id     = details.org_id
-    project_id = details.project_id
-  } }
+  value = { for key, details in harness_platform_input_set.inputset : key =>
+    {
+      identifier = details.identifier
+      org_id     = details.org_id
+      project_id = details.project_id
+    }
+  }
 }
-/* output "trigger" {
-  value = { for key, details in harness_platform_triggers.trigger : key => {
-    identifier = details.identifier
-    org_id     = details.org_id
-    project_id = details.project_id
-  } }
-} */
+output "verbose" {
+  value = { for key, details in local.inpt_by_svc : key => details }
+}
+output "verbose_by_infra" {
+  value = { for key, details in local.inpt_by_infra : key => details }
+}
