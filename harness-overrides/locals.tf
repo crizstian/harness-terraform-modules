5f2d398cf550b509335efc64210925e4c92f4ceb
root <cristiano.rosetti@gmail.com>
2023-06-09 18:35:54 +0000

    refactor

--- harness-overrides/locals.tf
+++ harness-overrides/locals.tf
@@ -1,21 +1,18 @@
 locals {
-  environments_service_overrides_org_id = merge([for environment, values in var.harness_platform_environments : { for org, details in var.organizations : environment => details.identifier if lower(org) == lower(try(values.organization, "NOT_FOUND")) }]...)
-  environments_service_overrides_prj_id = merge([for environment, values in var.harness_platform_environments : { for prj, details in var.projects : environment => details.identifier if lower(prj) == lower(try(values.project, "NOT_FOUND")) }]...)
-
   environments_service_overrides = merge([
-    for svc, variables in var.harness_platform_services : {
+    for svc, variables in var.harness_platform_overrides : {
       for env, values in try(variables.OVERRIDES.ENV, {}) : "${svc}_${env}" => {
         vars = merge(
           values,
           {
             identifier = "${lower(replace("${svc}_${env}", "/[\\s-.]/", "_"))}_${var.suffix}"
-            org_id     = try(local.environments_service_overrides_org_id[env], "") != "" ? local.environments_service_overrides_org_id[env] : try(variables.SERVICE_DEFINITION.org_id, var.common_values.org_id)
-            project_id = try(local.environments_service_overrides_prj_id[env], "") != "" ? local.environments_service_overrides_prj_id[env] : try(variables.SERVICE_DEFINITION.project_id, var.common_values.project_id)
-            env_id     = "${lower(replace(env, "/[\\s-.]/", "_"))}_${var.suffix}"
-            service_id = "${lower(replace(svc, "/[\\s-.]/", "_"))}_${var.suffix}"
+            org_id     = var.environments[env].org_id
+            project_id = var.environments[env].project_id
+            env_id     = var.environments[env].identifier
+            service_id = var.services[svc].identifier
           }
         )
-      } if variables.SERVICE_DEFINITION.enable
+      }
     }
   ]...)
 }
