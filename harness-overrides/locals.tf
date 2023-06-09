c05a958bc0cc83bd4e1d55f2b03eb94161074637
root <cristiano.rosetti@gmail.com>
2023-06-09 05:19:56 +0000

    refactor

--- harness-overrides/locals.tf
+++ harness-overrides/locals.tf
@@ -9,8 +9,8 @@ locals {
           values,
           {
             identifier = "${lower(replace("${svc}_${env}", "/[\\s-.]/", "_"))}_${var.suffix}"
-            org_id     = try(local.environments_service_overrides_org_id[env], "") != "" ? local.environment_org_id[env] : try(variables.SERVICE_DEFINITION.org_id, var.common_values.org_id)
-            project_id = try(local.environments_service_overrides_prj_id[env], "") != "" ? local.environment_prj_id[env] : try(variables.SERVICE_DEFINITION.project_id, var.common_values.project_id)
+            org_id     = try(local.environments_service_overrides_org_id[env], "") != "" ? local.environments_service_overrides_org_id[env] : try(variables.SERVICE_DEFINITION.org_id, var.common_values.org_id)
+            project_id = try(local.environments_service_overrides_prj_id[env], "") != "" ? local.environments_service_overrides_prj_id[env] : try(variables.SERVICE_DEFINITION.project_id, var.common_values.project_id)
             env_id     = "${lower(replace(env, "/[\\s-.]/", "_"))}_${var.suffix}"
             service_id = "${lower(replace(svc, "/[\\s-.]/", "_"))}_${var.suffix}"
           }
