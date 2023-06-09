3916452b79fdaf60065f16514a642103293a59ea
root <cristiano.rosetti@gmail.com>
2023-06-09 05:31:28 +0000

    refactor

--- harness-overrides/main.tf
+++ harness-overrides/main.tf
@@ -3,7 +3,7 @@ resource "harness_platform_environment_service_overrides" "overrides" {
   identifier = each.value.vars.identifier
   org_id     = each.value.vars.org_id
   env_id     = each.value.vars.env_id
-  project_id = each.value.vars.project_id
+  /* project_id = each.value.vars.project_id */
   service_id = each.value.vars.service_id
   yaml       = can(each.value.vars.yaml) ? templatefile(each.value.vars.yaml, each.value.vars) : ""
 }
