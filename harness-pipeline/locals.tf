8f824207dfa6b4ab808d213469b2c4bec94aae95
root <cristiano.rosetti@gmail.com>
2023-06-12 23:59:38 +0000

    refactor

--- harness-pipeline/locals.tf
+++ harness-pipeline/locals.tf
@@ -28,14 +28,14 @@ locals {
       {
         for k, v in try(values.template) : k =>
         {
-          template_id      = try(var.templates.stages[v.template_name].identifier, "")
-          template_version = try(v.template_version, "")
+          template_id      = try(var.templates.stages[v.template_name].identifier, "NOT_DEFINED")
+          template_version = try(v.template_version, "NOT_DEFINED")
         } if v.type == "stage"
       },
       {
         pipeline = {
-          template_id      = try(var.templates.pipelines[values.template.pipeline.template_name].identifier, "")
-          template_version = try(values.template.pipeline.template_version, "")
+          template_id      = try(var.templates.pipelines[values.template.pipeline.template_name].identifier, "NOT_DEFINED")
+          template_version = try(values.template.pipeline.template_version, "NOT_DEFINED")
         }
       }
     )
