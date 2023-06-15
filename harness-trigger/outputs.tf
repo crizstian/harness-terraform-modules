13cfd5ab0ed485993d923f329f1ec848e9b0346d
root <cristiano.rosetti@gmail.com>
2023-06-15 04:17:33 +0000

    refactor

--- harness-trigger/outputs.tf
+++ harness-trigger/outputs.tf
@@ -6,10 +6,9 @@ output "verbose-infra" {
 }
 output "trigger" {
   value = { for key, details in harness_platform_triggers.trigger : key => {
-    identifier   = details.identifier
-    org_id       = details.org_id
-    project_id   = details.project_id
-    test         = "${local.trg_by_infra[key].svc}__${local.trg_by_infra[key].env}"
-    inputset_ids = local.trg_by_infra[key].TRIGGER_INPUTSET
+    identifier = details.identifier
+    org_id     = details.org_id
+    project_id = details.project_id
+    test       = local.trg_by_infra[key]
   } }
 }
