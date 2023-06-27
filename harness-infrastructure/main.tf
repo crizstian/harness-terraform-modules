b2a401b8a58cc2b4da089894578ad2ed218ab008
root <cristiano.rosetti@gmail.com>
2023-06-27 01:12:32 +0000

    refactor

--- harness-infrastructure/main.tf
+++ harness-infrastructure/main.tf
@@ -12,7 +12,7 @@ resource "harness_platform_environment" "environment" {
 
 resource "harness_platform_infrastructure" "infrastructure" {
   for_each    = local.infrastructures
-  name        = each.value.vars.name
+  name        = each.key
   description = each.value.vars.description
   identifier  = each.value.vars.identifier
   org_id      = each.value.vars.org_id
