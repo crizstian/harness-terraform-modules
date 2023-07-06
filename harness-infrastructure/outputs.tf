d4dcd09c34e116c00490b2996303ca02ea57562a
root <cristiano.rosetti@gmail.com>
2023-07-06 18:51:48 +0000

    refactor

--- harness-infrastructure/outputs.tf
+++ harness-infrastructure/outputs.tf
@@ -11,9 +11,10 @@ output "environment" {
 output "infrastructure" {
   value = { for key, details in harness_platform_infrastructure.infrastructure : key =>
     {
-      identifier = details.identifier
-      org_id     = details.org_id
-      project_id = details.project_id
+      identifier         = details.identifier
+      org_id             = details.org_id
+      project_id         = details.project_id
+      delegate_selectors = try(local.infrastructures[key].delegate_selectors, [])
     }
   }
 }
