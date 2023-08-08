9c4a07c5ad4f79e9eb12f3611401b2acb8247356
root <cristiano.rosetti@gmail.com>
2023-08-08 03:20:21 +0000

    refactor

--- harness-inputset/locals.tf
+++ harness-inputset/locals.tf
@@ -130,7 +130,7 @@ locals {
   inpt_by_all_infra = merge(flatten([
     for name, details in var.harness_platform_inputsets : [
       for svc, variables in var.harness_platform_services : {
-        for pipe, values in try(variables.PIPELINE, {}) : "${svc}_${name}_ALL" => {
+        for pipe, values in try(variables.vars.PIPELINE, {}) : "${svc}_${name}_ALL" => {
           vars = merge(
             local.inpt_by_svc["${svc}_${name}"],
             {
@@ -139,12 +139,12 @@ locals {
             },
             flatten([for env, env_details in var.environments : [
               for infra, infra_details in var.infrastructures : {
-                "${variables.SERVICE_DEFINITION.type}_${lower(env)}_infrastructure_id" = infra_details.identifier
+                "${variables.vars.type}_${lower(env)}_infrastructure_id" = infra_details.identifier
               } if infra_details.env_id == env_details.identifier
             ]])...
           )
         } if name == pipe && values.INPUTSET
-      } if variables.SERVICE_DEFINITION.enable
+      } if variables.vars.enable
     ] if details.enable && details.type == "ALL"
   ])...)
 
