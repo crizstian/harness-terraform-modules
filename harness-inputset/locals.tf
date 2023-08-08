eafb22ff9908fb67260dadd0ad195c2f51973f0f
root <cristiano.rosetti@gmail.com>
2023-08-08 02:52:45 +0000

    refactor

--- harness-inputset/locals.tf
+++ harness-inputset/locals.tf
@@ -1,7 +1,7 @@
 # github pipelines
 locals {
 
-  inpt_by_svc = merge(flatten([
+  /* inpt_by_svc = merge(flatten([
     for name, details in var.harness_platform_inputsets : [
       for svc, variables in var.harness_platform_services : [
         for pipe, values in try(variables.PIPELINE, {}) : {
@@ -30,6 +30,32 @@ locals {
         } #if values.enable
       ] if variables.SERVICE_DEFINITION.enable
     ] if details.enable
+  ])...) */
+
+  inpt_by_svc = merge(flatten([
+    for name, details in var.harness_platform_inputsets : [
+      for svc, variables in var.harness_platform_services : {
+        for pipe, values in try(variables.PIPELINE, {}) : "${svc}_${name}" =>
+        merge(
+          try(var.templates.stages[name].default_values, try(var.templates.pipelines[pipe].default_values, {})),
+          try(var.connectors.default_connectors, {}),
+          try(variables.CONNECTORS, {}),
+          try(details.vars.usergroups_required, false) ? { usergroups = var.usergroups } : {},
+          details,
+          var.pipelines[pipe].default_values,
+          {
+            svc                                               = "${svc}"
+            suffix                                            = var.suffix
+            tags                                              = concat(try(variables.SERVICE_DEFINITION.tags, []), var.tags)
+            git_details                                       = try(variables.SERVICE_DEFINITION.git_details, {})
+            "${variables.SERVICE_DEFINITION.type}_service_id" = try("${replace(svc, "-", "_")}_${var.suffix}", "")
+            org_id                                            = try(var.pipelines[pipe].org_id, "") != "" ? var.pipelines[pipe].org_id : try(details.org_id, var.org_id)
+            project_id                                        = try(var.pipelines[pipe].project_id, "") != "" ? var.pipelines[pipe].project_id : try(details.project_id, var.project_id)
+            pipeline_id                                       = try(var.pipelines[pipe].identifier, "")
+          }
+        )
+      } if variables.SERVICE_DEFINITION.enable && values.INPUSET
+    ] if details.enable
   ])...)
 
   ci = { for name, values in local.inpt_by_svc : name =>
@@ -37,40 +63,40 @@ locals {
       vars = merge(
         values,
         {
-          name       = "${values.svc}_${values.inpt}"
+          name       = lower(replace(name, "/[\\s-.]/", "_"))
           identifier = "${lower(replace(name, "/[\\s-.]/", "_"))}_${var.suffix}"
         }
       )
     } if values.type == "CI"
   }
 
-  /* inpt_by_infra = merge(flatten([
+  inpt_by_infra = merge(flatten([
     for name, details in var.harness_platform_inputsets : [
       for svc, variables in var.harness_platform_services : [
+
         for pipe, values in try(variables.PIPELINE, {}) : [
-          for inpt, set in try(values.INPUTSET, {}) : {
-            for env, infra in variables.CD.ENV : "${svc}_${name}_${inpt}_${env}" =>
+          for env, env_details in var.environments : {
+            for infra, infra_details in var.infrastructures : "${svc}_${name}_${env}_${infra}" =>
             {
               vars = merge(
-                infra,
-                local.inpt_by_svc["${svc}_${name}_${inpt}"],
+                local.inpt_by_svc["${svc}_${name}"],
                 {
                   env                                                      = "${env}"
-                  env_id                                                   = var.environments[env].identifier
-                  "${variables.SERVICE_DEFINITION.type}_infrastructure_id" = var.infrastructures["${variables.SERVICE_DEFINITION.type}_${infra.infrastructure}"].identifier
-                  delegate_selectors                                       = try(var.infrastructures["${variables.SERVICE_DEFINITION.type}_${infra.infrastructure}"].delegate_selectors, ["NOT_DEFINED"])
-                  name                                                     = "${svc}_${env}"
-                  identifier                                               = "${lower(replace("${svc}_${name}_${inpt}_${env}", "/[\\s-.]/", "_"))}_${var.suffix}"
+                  env_id                                                   = env_details.identifier
+                  "${variables.SERVICE_DEFINITION.type}_infrastructure_id" = infra_details.identifier
+                  delegate_selectors                                       = try(infra_details.delegate_selectors, ["NOT_DEFINED"])
+                  name                                                     = replace("${svc}_${infra}", "kubernetes_", "")
+                  identifier                                               = "${lower(replace(replace("${svc}_${infra}", "/[\\s-.]/", "_"), "kubernetes_", ""))}_${var.suffix}"
                 }
               )
-            } if infra.enable && (lower(var.environments[env].type) == lower(set.type) || set.type == "all")
-          } if try(set.enable, false) && name == pipe
-        ] #if values.enable
+            } if infra_details.env_id == env_details.identifier
+          }
+        ] if name == pipe && values.INPUSET
       ] if variables.SERVICE_DEFINITION.enable
     ] if details.enable && details.type == "CD"
-  ])...) */
+  ])...)
 
-  inpt_by_infra = merge(flatten([
+  /* inpt_by_infra = merge(flatten([
     for name, details in var.harness_platform_inputsets : [
       for svc, variables in var.harness_platform_services : [
 
@@ -98,30 +124,30 @@ locals {
         ] #if values.enable
       ] if variables.SERVICE_DEFINITION.enable
     ] if details.enable && details.type == "CD"
-  ])...)
-
-  /* inpt_by_all_infra = merge(flatten([
-    for name, details in var.harness_platform_inputsets : [
-      for svc, variables in var.harness_platform_services : [
-        for pipe, values in try(variables.PIPELINE, {}) : {
-          for inpt, set in try(values.INPUTSET, {}) : "${svc}_${name}_${inpt}_ALL" => {
-            vars = merge(
-              local.inpt_by_svc["${svc}_${name}_${inpt}"],
-              {
-                name       = "${svc}"
-                identifier = "${lower(replace("${svc}_${name}_${inpt}_ALL", "/[\\s-.]/", "_"))}_${var.suffix}"
-              },
-              [for env, infra in variables.CD.ENV : {
-                "${variables.SERVICE_DEFINITION.type}_${lower(env)}_infrastructure_id" = var.infrastructures["${variables.SERVICE_DEFINITION.type}_${infra.infrastructure}"].identifier
-              }]...
-            )
-          } if try(set.enable, false) && name == pipe
-        } #if values.enable
-      ] if variables.SERVICE_DEFINITION.enable
-    ] if details.enable && details.type == "ALL"
   ])...) */
 
   inpt_by_all_infra = merge(flatten([
+    for name, details in var.harness_platform_inputsets : [
+      for svc, variables in var.harness_platform_services : {
+        for pipe, values in try(variables.PIPELINE, {}) : "${svc}_${name}_${inpt}_ALL" => {
+          vars = merge(
+            local.inpt_by_svc["${svc}_${name}"],
+            {
+              name       = "${svc}"
+              identifier = "${lower(replace("${svc}_${name}_ALL", "/[\\s-.]/", "_"))}_${var.suffix}"
+            },
+            flatten([for env, env_details in var.environments : [
+              for infra, infra_details in var.infrastructures : {
+                "${variables.SERVICE_DEFINITION.type}_${lower(env)}_infrastructure_id" = infra_details.identifier
+              } if infra_details.env_id == env_details.identifier
+            ]])...
+          )
+        } if name == pipe && values.INPUSET
+      } if variables.SERVICE_DEFINITION.enable
+    ] if details.enable && details.type == "ALL"
+  ])...)
+
+  /* inpt_by_all_infra = merge(flatten([
     for name, details in var.harness_platform_inputsets : [
       for svc, variables in var.harness_platform_services : [
         for pipe, values in try(variables.PIPELINE, {}) : {
@@ -142,7 +168,7 @@ locals {
         } #if values.enable
       ] if variables.SERVICE_DEFINITION.enable
     ] if details.enable && details.type == "ALL"
-  ])...)
+  ])...) */
 
   inputsets = merge(
     local.ci,
--- harness-service/output.tf
+++ harness-service/output.tf
@@ -1,9 +1,10 @@
 output "services" {
   value = { for key, details in harness_platform_service.services : key =>
-    {
-      identifier = details.identifier
-      org_id     = details.org_id
-      project_id = details.project_id
-    }
+    merge(
+      local.services[key],
+      {
+        identifier = details.identifier
+      }
+    )
   }
 }
