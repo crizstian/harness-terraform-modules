b567f3de74da69ae6678b170f39c38912b6374f2
root <cristiano.rosetti@gmail.com>
2023-09-06 19:15:25 +0000

    refactor

--- harness-policy/main.tf
+++ harness-policy/main.tf
@@ -6,5 +6,17 @@ resource "harness_platform_policy" "policy" {
   org_id      = each.value.org_id
   project_id  = each.value.project_id
   tags        = each.value.tags
-  rego        = file("${path.root}/${each.value.file}")
+  rego        = file(each.value.file)
 }
+
+/* resource "harness_platform_policyset" "test" {
+  identifier = "harness_platform_policyset.test.identifier"
+  name       = "harness_platform_policyset.test.name"
+  action     = "onrun"
+  type       = "pipeline"
+  enabled    = true
+  policies {
+    identifier = "policy_identifier"
+    severity   = "warning"
+  }
+} */
--- harness-policy/variables.tf
+++ harness-policy/variables.tf
@@ -2,14 +2,19 @@ variable "suffix" {}
 variable "tags" {
   default = []
 }
-variable "harness_platform_policies" {
-  description = "Harness policies to be created in the given Harness account"
-  default     = {}
-}
 variable "org_id" {
   default = ""
 }
 variable "project_id" {
+  default = ""
+}
+variable "organizations" {
+  default = {}
+}
+variable "projects" {
+  default = {}
+}
+variable "tags" {
   default = ""
 }
 variable "common_values" {
@@ -18,15 +23,12 @@ variable "common_values" {
     project_id = ""
   }
 }
-locals {
-  harness_policies = { for name, details in var.harness_platform_policies : name => merge(
-    details,
-    {
-      org_id      = try(details.org_id, var.org_id)
-      project_id  = try(details.project_id, var.project_id)
-      tags        = concat(try(details.tags, []), var.tags)
-      identifier  = "${lower(replace(name, "-", "_"))}_template_${var.suffix}"
-      description = details.description
-    }
-  ) }
+
+variable "harness_platform_policies" {
+  description = "Harness policies to be created in the given Harness account"
+  default     = {}
+}
+variable "harness_platform_policy_sets" {
+  description = "Harness policies to be created in the given Harness account"
+  default     = {}
 }
