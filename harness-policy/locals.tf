ba55a8382924f1627b5fea7ea16f03123280b6ac
root <cristiano.rosetti@gmail.com>
2023-09-06 19:18:25 +0000

    refactor

---
+++ harness-policy/locals.tf
@@ -0,0 +1,21 @@
+locals {
+  policy_org_id = merge([for policy, values in var.harness_platform_policies : { for org, details in var.organizations : policy => details.identifier if lower(org) == lower(try(values.organization, "NOT_FOUND")) }]...)
+  policy_prj_id = merge([for policy, values in var.harness_platform_policies : { for prj, details in var.projects : policy => details.identifier if lower(prj) == lower(try(values.project, "NOT_FOUND")) }]...)
+
+  harness_policies = {
+    for name, details in var.harness_platform_policies : name => merge(
+      details,
+      {
+        name        = "${name}"
+        identifier  = "${lower(replace(name, "/[\\s-.]/", "_"))}_${var.suffix}"
+        tags        = concat(try(details.vars.tags, []), var.tags)
+        org_id      = try(local.policy_org_id[name], "") != "" ? local.policy_org_id[name] : try(details.org_id, var.common_values.org_id)
+        project_id  = try(local.policy_prj_id[name], "") != "" ? local.policy_prj_id[name] : try(details.project_id, var.common_values.project_id)
+        description = details.description
+      }
+    )
+  }
+
+  policy_sets_org_id = merge([for policy, values in var.harness_platform_policy_sets : { for org, details in var.organizations : policy => details.identifier if lower(org) == lower(try(values.organization, "NOT_FOUND")) }]...)
+  policy_sets_prj_id = merge([for policy, values in var.harness_platform_policy_sets : { for prj, details in var.projects : policy => details.identifier if lower(prj) == lower(try(values.project, "NOT_FOUND")) }]...)
+}
