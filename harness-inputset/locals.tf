2718c844d4579efe5004d29be74b8ab104d026f1
root <cristiano.rosetti@gmail.com>
2023-09-14 05:49:17 +0000

    refactor

--- harness-inputset/locals.tf
+++ harness-inputset/locals.tf
@@ -15,7 +15,7 @@ locals {
   inpt_by_svc = merge(flatten([
     for name, details in var.harness_platform_inputsets : [
       for svc, variables in var.services : {
-        for pipe, values in variables.PIPELINE : "${svc}_${name}" =>
+        for pipe, values in variables.vars.PIPELINE : "${svc}_${name}" =>
         merge(
           try(details.vars.usergroups_required, false) ? { usergroups = var.usergroups } : {},
           try(var.templates.pipelines[pipe].default_values, {}),
@@ -26,15 +26,15 @@ locals {
           {
             svc         = "${svc}"
             suffix      = var.suffix
-            tags        = concat(try(variables.tags, []), var.tags)
-            git_details = try(variables.git_details, {})
+            tags        = concat(try(variables.vars.tags, []), var.tags)
+            git_details = try(variables.vars.git_details, {})
             org_id      = var.pipelines[pipe].org_id
             project_id  = var.pipelines[pipe].project_id
             pipeline_id = var.pipelines[pipe].identifier
           },
           details
         ) if values.INPUTSET && try(details.pipeline, name) == pipe
-      }
+      } if variables.vars.enable
     ] if details.enable
   ])...)
 
@@ -54,26 +54,26 @@ locals {
     for name, details in var.harness_platform_inputsets : [
       for svc, variables in var.services : [
 
-        for pipe, values in variables.PIPELINE : [
+        for pipe, values in variables.vars.PIPELINE : [
           for env, env_details in var.environments : {
             for infra, infra_details in var.infrastructures : "${svc}_${name}_${env}_${infra}" =>
             {
               vars = merge(
                 local.inpt_by_svc["${svc}_${name}"],
                 {
-                  env                                   = "${env}"
-                  env_id                                = env_details.identifier
-                  primary_artifact                      = env_details.primary_artifact
-                  delegate_selectors                    = try(infra_details.delegate_selectors, ["NOT_DEFINED"])
-                  name                                  = replace("${svc}_${infra}", "${variables.type}_", "")
-                  identifier                            = "${lower(replace(replace("${svc}_${infra}", "/[\\s-.]/", "_"), "${variables.type}_", ""))}_${var.suffix}"
-                  "${variables.type}_infrastructure_id" = infra_details.identifier
+                  env                                        = "${env}"
+                  env_id                                     = env_details.identifier
+                  primary_artifact                           = env_details.primary_artifact
+                  delegate_selectors                         = try(infra_details.delegate_selectors, ["NOT_DEFINED"])
+                  name                                       = replace("${svc}_${infra}", "${variables.vars.type}_", "")
+                  identifier                                 = "${lower(replace(replace("${svc}_${infra}", "/[\\s-.]/", "_"), "${variables.vars.type}_", ""))}_${var.suffix}"
+                  "${variables.vars.type}_infrastructure_id" = infra_details.identifier
                 }
               )
             } if infra_details.env_id == env_details.identifier
-          } if contains(keys(variables.artifacts), env_details.primary_artifact)
+          } if contains(keys(variables.vars.artifacts), env_details.primary_artifact)
         ] if try(details.pipeline, name) == pipe && values.INPUTSET
-      ]
+      ] if variables.vars.enable
     ] if details.enable && details.type == "CD"
   ])...)
 
@@ -82,14 +82,14 @@ locals {
     for name, details in var.harness_platform_inputsets : [
       for svc, variables in var.services : [
 
-        for pipe, values in variables.PIPELINE : [
+        for pipe, values in variables.vars.PIPELINE : [
           for env, env_details in var.environments : {
             for infra, infra_details in var.infrastructures : "${svc}_${name}_${env}_${infra}" => local.inpt["${svc}_${name}_${env}_${infra}"]
 
-            if infra_details.env_id == env_details.identifier && !can(variables.settings.infrastructure)
-          } if contains(keys(variables.artifacts), env_details.primary_artifact) && !can(variables.settings.environments) #&& try(local.inpt_by_svc["${svc}_${name}"].environment_type, env_details.type) == env_details.type
-        ] if try(details.pipeline, name) == pipe && values.INPUTSET && !can(variables.settings.pipelines)
-      ] if !can(variables.settings.inputsets)
+            if infra_details.env_id == env_details.identifier && !can(variables.vars.settings.infrastructure)
+          } if contains(keys(variables.vars.artifacts), env_details.primary_artifact) && !can(variables.vars.settings.environments) #&& try(local.inpt_by_svc["${svc}_${name}"].environment_type, env_details.type) == env_details.type
+        ] if try(details.pipeline, name) == pipe && values.INPUTSET && !can(variables.vars.settings.pipelines)
+      ] if variables.vars.enable && !can(variables.vars.settings.inputsets)
     ] if details.enable && details.type == "CD"
   ])...)
 
@@ -97,14 +97,14 @@ locals {
     for name, details in var.harness_platform_inputsets : [
       for svc, variables in var.services : [
 
-        for pipe, values in variables.PIPELINE : [
+        for pipe, values in variables.vars.PIPELINE : [
           for env, env_details in var.environments : {
             for infra, infra_details in var.infrastructures : "${svc}_${name}_${env}_${infra}" => local.inpt["${svc}_${name}_${env}_${infra}"]
 
-            if infra_details.env_id == env_details.identifier && !can(variables.settings.infrastructure)
-          } if contains(keys(variables.artifacts), env_details.primary_artifact) && !can(variables.settings.environments) #&& try(local.inpt_by_svc["${svc}_${name}"].environment_type, env_details.type) == env_details.type
-        ] if try(details.pipeline, name) == pipe && values.INPUTSET && !can(variables.settings.pipelines)
-      ] if try(variables.vars.settings.inputsets[name], false)
+            if infra_details.env_id == env_details.identifier && !can(variables.vars.settings.infrastructure)
+          } if contains(keys(variables.vars.artifacts), env_details.primary_artifact) && !can(variables.vars.settings.environments) #&& try(local.inpt_by_svc["${svc}_${name}"].environment_type, env_details.type) == env_details.type
+        ] if try(details.pipeline, name) == pipe && values.INPUTSET && !can(variables.vars.settings.pipelines)
+      ] if variables.vars.enable && try(variables.vars.vars.settings.inputsets[name], false)
     ] if details.enable && details.type == "CD"
   ])...)
 
@@ -112,14 +112,14 @@ locals {
     for name, details in var.harness_platform_inputsets : [
       for svc, variables in var.services : [
 
-        for pipe, values in variables.PIPELINE : [
+        for pipe, values in variables.vars.PIPELINE : [
           for env, env_details in var.environments : {
             for infra, infra_details in var.infrastructures : "${svc}_${name}_${env}_${infra}" => local.inpt["${svc}_${name}_${env}_${infra}"]
 
-            if infra_details.env_id == env_details.identifier && try(variables.vars.settings.infrastructure[replace(infra, "${variables.type}_", "")], false)
-          } if contains(keys(variables.artifacts), env_details.primary_artifact) && !can(variables.settings.environments) #&& try(local.inpt_by_svc["${svc}_${name}"].environment_type, env_details.type) == env_details.type
-        ] if try(details.pipeline, name) == pipe && values.INPUTSET && !can(variables.settings.pipelines)
-      ] if !can(variables.settings.inputsets)
+            if infra_details.env_id == env_details.identifier && try(variables.vars.vars.settings.infrastructure[replace(infra, "${variables.vars.type}_", "")], false)
+          } if contains(keys(variables.vars.artifacts), env_details.primary_artifact) && !can(variables.vars.settings.environments) #&& try(local.inpt_by_svc["${svc}_${name}"].environment_type, env_details.type) == env_details.type
+        ] if try(details.pipeline, name) == pipe && values.INPUTSET && !can(variables.vars.settings.pipelines)
+      ] if variables.vars.enable && !can(variables.vars.settings.inputsets)
     ] if details.enable && details.type == "CD"
   ])...)
 
@@ -129,14 +129,14 @@ locals {
     for name, details in var.harness_platform_inputsets : [
       for svc, variables in var.services : [
 
-        for pipe, values in variables.PIPELINE : [
+        for pipe, values in variables.vars.PIPELINE : [
           for env, env_details in var.environments : {
             for infra, infra_details in var.infrastructures : "${svc}_${name}_${env}_${infra}" => local.inpt["${svc}_${name}_${env}_${infra}"]
 
-            if infra_details.env_id == env_details.identifier && !can(variables.settings.infrastructure)
-          } if contains(keys(variables.artifacts), env_details.primary_artifact) && !can(variables.settings.environments) #&& try(local.inpt_by_svc["${svc}_${name}"].environment_type, env_details.type) == env_details.type
-        ] if try(details.pipeline, name) == pipe && values.INPUTSET && try(variables.vars.settings.pipelines[try(details.pipeline, name)], false)
-      ] if !can(variables.settings.inputsets)
+            if infra_details.env_id == env_details.identifier && !can(variables.vars.settings.infrastructure)
+          } if contains(keys(variables.vars.artifacts), env_details.primary_artifact) && !can(variables.vars.settings.environments) #&& try(local.inpt_by_svc["${svc}_${name}"].environment_type, env_details.type) == env_details.type
+        ] if try(details.pipeline, name) == pipe && values.INPUTSET && try(variables.vars.vars.settings.pipelines[try(details.pipeline, name)], false)
+      ] if variables.vars.enable && !can(variables.vars.settings.inputsets)
     ] if details.enable && details.type == "CD"
   ])...)
 
@@ -145,14 +145,14 @@ locals {
     for name, details in var.harness_platform_inputsets : [
       for svc, variables in var.services : [
 
-        for pipe, values in variables.PIPELINE : [
+        for pipe, values in variables.vars.PIPELINE : [
           for env, env_details in var.environments : {
             for infra, infra_details in var.infrastructures : "${svc}_${name}_${env}_${infra}" => local.inpt["${svc}_${name}_${env}_${infra}"]
 
-            if infra_details.env_id == env_details.identifier && try(variables.vars.settings.infrastructure[replace(infra, "${variables.type}_", "")], false)
-          } if contains(keys(variables.artifacts), env_details.primary_artifact) && !can(variables.settings.environments) #&& try(local.inpt_by_svc["${svc}_${name}"].environment_type, env_details.type) == env_details.type
-        ] if try(details.pipeline, name) == pipe && values.INPUTSET && try(variables.vars.settings.pipelines[try(details.pipeline, name)], false)
-      ] if !can(variables.settings.inputsets)
+            if infra_details.env_id == env_details.identifier && try(variables.vars.vars.settings.infrastructure[replace(infra, "${variables.vars.type}_", "")], false)
+          } if contains(keys(variables.vars.artifacts), env_details.primary_artifact) && !can(variables.vars.settings.environments) #&& try(local.inpt_by_svc["${svc}_${name}"].environment_type, env_details.type) == env_details.type
+        ] if try(details.pipeline, name) == pipe && values.INPUTSET && try(variables.vars.vars.settings.pipelines[try(details.pipeline, name)], false)
+      ] if variables.vars.enable && !can(variables.vars.settings.inputsets)
     ] if details.enable && details.type == "CD"
   ])...)
 
@@ -160,14 +160,14 @@ locals {
     for name, details in var.harness_platform_inputsets : [
       for svc, variables in var.services : [
 
-        for pipe, values in variables.PIPELINE : [
+        for pipe, values in variables.vars.PIPELINE : [
           for env, env_details in var.environments : {
             for infra, infra_details in var.infrastructures : "${svc}_${name}_${env}_${infra}" => local.inpt["${svc}_${name}_${env}_${infra}"]
 
-            if infra_details.env_id == env_details.identifier && try(variables.vars.settings.infrastructure[replace(infra, "${variables.type}_", "")], false)
-          } if contains(keys(variables.artifacts), env_details.primary_artifact) && !can(variables.settings.environments) #&& try(local.inpt_by_svc["${svc}_${name}"].environment_type, env_details.type) == env_details.type
-        ] if try(details.pipeline, name) == pipe && values.INPUTSET && try(variables.vars.settings.pipelines[try(details.pipeline, name)], false)
-      ] if try(variables.vars.settings.inputsets[name], false)
+            if infra_details.env_id == env_details.identifier && try(variables.vars.vars.settings.infrastructure[replace(infra, "${variables.vars.type}_", "")], false)
+          } if contains(keys(variables.vars.artifacts), env_details.primary_artifact) && !can(variables.vars.settings.environments) #&& try(local.inpt_by_svc["${svc}_${name}"].environment_type, env_details.type) == env_details.type
+        ] if try(details.pipeline, name) == pipe && values.INPUTSET && try(variables.vars.vars.settings.pipelines[try(details.pipeline, name)], false)
+      ] if variables.vars.enable && try(variables.vars.vars.settings.inputsets[name], false)
     ] if details.enable && details.type == "CD"
   ])...)
 
@@ -175,7 +175,7 @@ locals {
   inpt_by_all_infra = merge(flatten([
     for name, details in var.harness_platform_inputsets : [
       for svc, variables in var.services : {
-        for pipe, values in variables.PIPELINE : "${svc}_${name}_ALL" => {
+        for pipe, values in variables.vars.PIPELINE : "${svc}_${name}_ALL" => {
           vars = merge(
             local.inpt_by_svc["${svc}_${name}"],
             {
@@ -184,19 +184,19 @@ locals {
             },
             flatten([for env, env_details in var.environments : [
               for infra, infra_details in var.infrastructures : {
-                "${variables.type}_${lower(env)}_infrastructure_id" = infra_details.identifier
+                "${variables.vars.type}_${lower(env)}_infrastructure_id" = infra_details.identifier
               } if infra_details.env_id == env_details.identifier
             ]])...
           )
         } if try(details.pipeline, name) == pipe && values.INPUTSET
-      } if !can(variables.settings.infrastructure)
+      } if variables.vars.enable && !can(variables.vars.settings.infrastructure)
     ] if details.enable && details.type == "ALL" && !can(details.vars.base_env)
   ])...)
 
   inpt_by_base_env = merge(flatten([
     for name, details in var.harness_platform_inputsets : [
       for svc, variables in var.services : {
-        for pipe, values in variables.PIPELINE : "${svc}_${name}_ALL" => {
+        for pipe, values in variables.vars.PIPELINE : "${svc}_${name}_ALL" => {
           vars = merge(
             local.inpt_by_svc["${svc}_${name}"],
             {
@@ -205,12 +205,12 @@ locals {
             },
             flatten([for env, env_details in var.environments : [
               for infra, infra_details in var.infrastructures : {
-                "${variables.type}_${lower(env)}_infrastructure_id" = infra_details.identifier
+                "${variables.vars.type}_${lower(env)}_infrastructure_id" = infra_details.identifier
               } if infra_details.env_id == env_details.identifier && details.vars.base_env == env
             ]])...
           )
         } if try(details.pipeline, name) == pipe && values.INPUTSET
-      } if !can(variables.settings.infrastructure)
+      } if variables.vars.enable && !can(variables.vars.settings.infrastructure)
     ] if details.enable && details.type == "ALL" && can(details.vars.base_env)
   ])...)
 
