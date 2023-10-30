3aecfe55a0ab2b9f0e90afdd5d743058607d962a
root <cristiano.rosetti@gmail.com>
2023-10-30 14:29:39 +0000

    kubernetes_ccm_connectors

--- harness-ccm/kubernetes/manifest.tf
+++ harness-ccm/kubernetes/manifest.tf
@@ -3,7 +3,6 @@ resource "kubernetes_manifest" "autostoppping-crd" {
 }
 
 data "kubernetes_service_v1" "metrics_server" {
-  count = strcontains(terraform.workspace, "anthos") ? 0 : 1
   metadata {
     name      = "metrics-server"
     namespace = "kube-system"
