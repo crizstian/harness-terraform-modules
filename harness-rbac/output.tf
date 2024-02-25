3ad1eba7806df62c358bc64cb6f656cee0042ec2
cristian.ramirez@harness.io <cristiano.rosetti@gmail.com>
2024-02-24 21:27:53 -0600

    update

--- harness-rbac/output.tf
+++ harness-rbac/output.tf
@@ -1,3 +1,12 @@
 output "roles" {
   value = { for k, v in harness_platform_roles.role : k => v.identifier }
 }
+output "users" {
+  value = { for k, v in harness_platform_user.user : k => v.identifier }
+}
+output "usergroups" {
+  value = { for k, v in harness_platform_usergroup.usergroup : k => v.identifier }
+}
+output "service_accounts" {
+  value = { for k, v in harness_platform_service_account.service_account : k => v.identifier }
+}
