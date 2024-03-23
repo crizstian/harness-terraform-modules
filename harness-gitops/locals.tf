2c085f0a342c1310fc35e5c5569198252d47637b
cristian.ramirez@harness.io <cristiano.rosetti@gmail.com>
2024-03-23 15:00:37 -0600

    refactor

--- harness-gitops/locals.tf
+++ harness-gitops/locals.tf
@@ -2,8 +2,7 @@ locals {
   gitops_org_id = merge([for gitops, values in var.harness_platform_gitops_applications : { for org, details in var.organizations : gitops => details.identifier if lower(org) == lower(try(values.organization, "")) }]...)
   gitops_prj_id = merge([for gitops, values in var.harness_platform_gitops_applications : { for prj, details in var.projects : gitops => details.identifier if lower(prj) == lower(try(values.project, "")) }]...)
 
-  gitops_agents = { for app, details in var.harness_platform_gitops_agent : app => {
-    vars = merge(
+  gitops_agents = { for app, details in var.harness_platform_gitops_agent : app => merge(
       details,
       {
         identifier    = "${lower(replace(app, "/[\\s-.]/", "_"))}_${var.suffix}"
@@ -11,7 +10,7 @@ locals {
         org_id        = try(local.gitops_org_id[app], "") != "" ? local.gitops_org_id[app] : try(details.org_id, var.common_values.org_id)
         project_id    = try(local.gitops_prj_id[app], "") != "" ? local.gitops_prj_id[app] : try(details.project_id, var.common_values.project_id)
       }
-  ) } if details.enable }
+  ) if details.enable }
 
   gitops_cluster = { for app, details in var.harness_platform_gitops_cluster : app => {
     vars = merge(
